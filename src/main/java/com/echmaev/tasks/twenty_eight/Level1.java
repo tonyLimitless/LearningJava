0cde7628b716143977a470abd9a92672998fc6d4
Anton Echmaev <me@echmaev.ru>
2023-04-19 22:10:50 +0500

    28/Level1 add case for 0

--- src/main/java/com/echmaev/tasks/twenty_eight/Level1.java
+++ src/main/java/com/echmaev/tasks/twenty_eight/Level1.java
@@ -4,11 +4,15 @@ import java.util.*;
 
 public class Level1 {
     public static int squirrel(int n) {
-        int newValue = n;
-        for (int counter = n; counter > 1; counter--) {
-            newValue = newValue * (counter - 1);
+        if (n > 0) {
+            int newValue = n;
+            for (int counter = n; counter > 1; counter--) {
+                newValue = newValue * (counter - 1);
+            }
+            String number = String.valueOf(newValue);
+            return Integer.valueOf(String.valueOf(number.toCharArray()[0]));
+        } else {
+            return 1;
         }
-        String number = String.valueOf(newValue);
-        return Integer.valueOf(String.valueOf(number.toCharArray()[0]));
     }
 }
