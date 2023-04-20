b1a13967fea7ddd10b60ad2d97bfa8153f3f7893
Anton Echmaev <me@echmaev.ru>
2023-04-20 13:14:48 +0500

    28/Level2

---
+++ src/main/java/com/echmaev/tasks/twenty_eight/Level2.java
@@ -0,0 +1,29 @@
+package com.echmaev.tasks.twenty_eight;
+
+public class Level2 {
+    public static int odometer(int[] oksana) {
+        if (oksana.length % 2 != 0 || oksana.length < 2) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            int countOfActivities = oksana.length / 2;
+            int km;
+            if(countOfActivities == 1) {
+                km = oksana[0] * oksana[1];
+                return km;
+            } else {
+                km = oksana[0] * oksana[1];
+                while (countOfActivities > 1) {
+                    km += (oksana[countOfActivities * 2 - 1] - oksana[countOfActivities * 2 - 3])
+                            * oksana[countOfActivities * 2 - 2];
+                    countOfActivities--;
+                }
+                System.out.println(km);
+            }
+            return km;
+        }
+    }
+}
