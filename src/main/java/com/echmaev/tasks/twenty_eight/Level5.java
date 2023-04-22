2393b95ed1ed71d8fb27dd8fffc4351e63f66a0a
Anton Echmaev <me@echmaev.ru>
2023-04-22 20:44:27 +0500

    28 Восстановление таблицы зарплат

---
+++ src/main/java/com/echmaev/tasks/twenty_eight/Level5.java
@@ -0,0 +1,48 @@
+package com.echmaev.tasks.twenty_eight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Level1 {
+    public static int [] SynchronizingTables(int n, int [] ids, int [] salary) {
+
+        int[] arrayIds = new int[n];
+
+        for (int index = 0; index < n; index++) {
+            arrayIds[index] = ids[index];
+        }
+
+        Map<Integer, Integer> salaryForIdsMap = new HashMap<>(n);
+        sortOfChoice(ids);
+        sortOfChoice(salary);
+
+        for (int index = 0; index < n; index++) {
+            salaryForIdsMap.put(ids[index], salary[index]);
+        }
+
+        int[] newArray = new int[n];
+
+        for (int index = 0; index < n; index++) {
+            newArray[index] = salaryForIdsMap.get(arrayIds[index]);
+        }
+        return newArray;
+
+    }
+
+    public static int[] sortOfChoice(int[] tele) {
+        for (int i = 0; i < tele.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < tele.length; j++) {
+                if (tele[j] < tele[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = tele[i];
+                tele[i] = tele[minIndex];
+                tele[minIndex] = temp;
+            }
+        }
+        return tele;
+    }
+}
