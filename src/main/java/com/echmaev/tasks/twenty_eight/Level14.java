db50118658629ef6dbdfc6cc6661560832d2af64
Anton Echmaev <me@echmaev.ru>
2023-05-14 15:42:38 +0500

    Оптимизация беспилотного трафика

--- src/main/java/com/echmaev/tasks/twenty_eight/Level14.java
+++ src/main/java/com/echmaev/tasks/twenty_eight/Level14.java
@@ -2,33 +2,62 @@ package com.echmaev.tasks.twenty_eight;
 
 public class Level1 {
     public static int Unmanned(int L, int N, int[][] track) {
-        int time = 0; // текущее время
-        int position = 0; // текущая позиция автомобиля
+        String color = "";
+        int dist_gen = 0;
+        int gen_count = 0; // общий счетчик ходов
 
-        for (int i = 0; i < N; i++) {
-            // вычисляем время, через которое автомобиль достигнет i-го светофора
-            int arrivalTime = track[i][0];
+        for (int k = 0; k < track.length; k++) { // отрезков действия столько сколько светофоров
+            int time_light = 0; // счетчик горения светофора? обнуляем при переходе к след светофору
+            int wait_time = 0; // время ожидания на светофоре
+            int time_drive = 0;
 
-            // вычисляем количество циклов светофора, которые пройдет автомобиль до прибытия
-            int cycles = (arrivalTime - position) / (track[i][1] + track[i][2]);
+            while (true) {
+                time_drive += 1;
+                gen_count += 1;
+                time_light += 1; // сначала загорается красный
 
-            // обновляем текущее время и позицию автомобиля
-            time += cycles * (track[i][1] + track[i][2]);
-            position += cycles * (track[i][1] + track[i][2]);
+                if (time_light > track[k][1] + track[k][2]) { // зацикливаем бесконечное горение светоф
+                    time_light = 1;
+                }
 
-            // проверяем, на какой цвет переключится светофор в момент прибытия автомобиля
-            if (position < arrivalTime) {
-                position = arrivalTime;
-            } else if (position >= arrivalTime && position < arrivalTime + track[i][1]) {
-                time += track[i][1] - (position - arrivalTime);
-                position = arrivalTime + track[i][1];
+                if (time_light <= track[k][1]) {
+                    color = "red"; // то горит красный
+                }
+
+                if (time_light > track[k][1] && time_light <= track[k][1] + track[k][2]) {
+                    color = "green";
+                }
+
+                if (color.equals("green") && k == 0 && track.length > 1) {
+                    dist_gen = track[k][0] + wait_time + (track[k + 1][0] - track[k][0]);
+                    break;
+                }
+
+                if (gen_count > track[k][0] && color.equals("red") && k == 0) { // k==0 заглушка для работы только на 1 светофоре
+                    wait_time += 1; // необходимо сделать чтобы было 2
+                }
+
+                if (time_drive > dist_gen && color.equals("red") && k > 0) {
+                    wait_time += 1;
+                }
+
+                if (time_drive >= dist_gen && color.equals("green") && k < track.length - 1) {
+                    dist_gen = dist_gen + wait_time + (track[k + 1][0] - track[k][0]);
+                    break;
+                }
+
+                if (time_drive >= dist_gen && color.equals("green") && k == track.length - 1 && track.length > 1) {
+                    dist_gen = dist_gen + wait_time + (L - track[k][0]);
+                    return dist_gen;
+                }
+
+                if (color.equals("green") && track.length == 1) {
+                    dist_gen = track[k][0] + wait_time + (L - track[k][0]);
+                    return dist_gen;
+                }
             }
         }
 
-        // вычисляем оставшееся время до конца дороги
-        int remainingTime = L - position;
-        time += remainingTime;
-
-        return time;
+        return dist_gen;
     }
 }
